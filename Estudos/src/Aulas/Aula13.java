17fc3e077db72648b4b266d489f6b482878187ac
Patricia Novaski Gavloski <128609847+PatriciaGavloski@users.noreply.github.com>
2024-07-09 10:18:15 -0300

    Add files via upload

---
+++ Estudos/src/Aulas/Aula13.java
@@ -0,0 +1,21 @@
+package Aulas;
+
+public class Aula13 {
+
+     public static void main(String[] args) {
+
+     }
+
+     public int somaMultiplica (int n, int x, String m){
+        int r = 0; // r é igual ao resultado 
+        if (m == "M"){ // M = multiplicação
+            r = n * x;
+     }else{
+        //se não soma mesmo 
+        r = n+ x;
+     }
+     return r;
+
+     }
+          
+}
---
+++ Estudos/src/Aulas/Aula14.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+package Aulas;
+public class Aula14 {
+    public static void main (String [] args) {
+
+Scanner scanner = new Scanner(System.in);
+
+System.out.println("Olá, seja muuito Bem-Vindo");
+System.out.println("Por Favor digite as informações a seguir!");
+
+System.out.println("Número da conta: ");      
+int numero = Scanner.nextLine();
+
+System.out.println("Número da Agência: ");     
+int agencia = Scanner.nextLine();
+
+System.out.println("Nome do cliente: ");     
+int nome = Scanner.nextLine();
+
+float saldo = 237.48;
+
+
+        System.out.println("Fim");
+
+    }
+    
+}
+
